006d4f289c6f91f652c9213cdfd720a9c3ffc4b4
Anne Wilga <anne.wilga@orange.fr>
2021-03-30 17:00:54 +0200

    trigger
    message d'erreurs :
    date_dernier_contact doit être >= à date_debut_transaction.
    date_transaction_fin doit être >= à date_debut_transaction.
    est_conclu doit valoir TRUE (1) ou FALSE (2).

--- trigger.sql
+++ trigger.sql
@@ -1,34 +1,35 @@
 -- Création de la table Erreur
 use wazaaimmogroup;
 DROP Table IF EXISTS Erreur;
-CREATE TABLE Erreur (
+CREATE TABLE Erreur 
+(
     id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    erreur VARCHAR(255) UNIQUE,
-    in_id int(10));
-
+    erreur VARCHAR(255) UNIQUE
+);
 -- Insertion de l'erreur qui nous intéresse
-INSERT INTO Erreur (erreur) VALUES ('Erreur : Veuillez recontactez votre client');
 INSERT INTO Erreur (erreur) VALUES ('Erreur : date_dernier_contact doit être >= à date_debut_reservation.');
-
+INSERT INTO Erreur (erreur) VALUES ('Erreur : date_transaction_fin doit être >= à date_debut_transaction.');
+INSERT INTO Erreur (erreur) VALUES ('Erreur : est_conclu doit valoir TRUE (1) ou FALSE (2).');
 -- Création du trigger
 DELIMITER |
-DROP TRIGGER if EXISTS date_different_contact |
-CREATE TRIGGER date_different_contact 
+DROP TRIGGER if EXISTS date_contact |
+CREATE TRIGGER date_contact 
 AFTER UPDATE 
 ON waz_negocier 
 FOR EACH ROW
 BEGIN
-    DECLARE date_different_contact INT;
-    DECLARE id_i INT;
-    SET date_different_contact = 14;
-    SET  id_i = in_id;
-    IF date_dernier_contact - date_debut_transaction >= date_different_contact
 
-      THEN
-        INSERT INTO Erreur (erreur,in_id) VALUES ('Erreur : Veuillez recontactez votre client',in_id);
-    ELSEIF date__dernier_contact < new.date_debut_transaction 
+    IF new.date_dernier_contact < old.date_debut_transaction 
+        THEN
         -- vérification des dates dans les champs  
-        INSERT INTO Erreur (erreur) VALUES ('Erreur : date_dernier_contact doit être >= à date_debut_reservation.');
+            INSERT INTO Erreur (erreur) VALUES ('Erreur : date_dernier_contact doit être >= à date_debut_transaction.');
+    ELSEIF new.date_transaction_fin < old.date_debut_transaction
+        THEN
+            INSERT INTO Erreur (erreur) VALUES ('Erreur : date_transaction_fin doit être >= à date_debut_transaction.');
+    ELSEIF new.est_conclu != TRUE -- ni true
+    AND new.est_conclu != FALSE -- ni false
+        THEN
+            INSERT INTO Erreur (erreur) VALUES ('Erreur : est_conclu doit valoir TRUE (1) ou FALSE (2).');
     END IF;
 END |
 DELIMITER ;
